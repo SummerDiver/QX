c1e938995400fa138d8a3b8c0218b22ec30ef9fb
Xia Yuang <xiayuang@foxmail.com>
2022-10-31 16:44:23 +0800

    pingan.conf

--- script/pingan/pingan.js
+++ script/pingan/pingan.js
@@ -5,7 +5,7 @@ const searchParams = new URLSearchParams(url.search)
 const controllerId = searchParams.get('controllerId')
 
 const inId = ['021', '1000006$7$0$0']
-const outId = ['030', '1000011$7$0$0', '1000000$7$0$0']
+const outId = ['030', '1000000$7$0$0', '1000011$7$0$0', '1000020$7$0$0']
 
 obj.status = 1
 obj.msg = `研究生用户，${outId.indexOf(controllerId) >= 0 ? `出` : `入`}校授权有效！`
