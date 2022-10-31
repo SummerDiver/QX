dad07264ccf465d929c126baf945a5352e40d96b
Xia Yuang <xiayuang@foxmail.com>
2022-10-31 15:32:58 +0800

    test.js

--- script/pingan/test.js
+++ script/pingan/test.js
@@ -1,9 +1,8 @@
 let url = $request.url
-
 let obj = JSON.parse($response.body)
 
 obj.status = 1
-obj.msg = `${url}`
+obj.msg = `研究生用户，${url.indexOf(`030`)}校授权有效！`
 
 const body = JSON.stringify(obj)
 $done({ body })
